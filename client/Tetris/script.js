fa8db3359d46c509cffb730a3fea68dac661ec61
HunterK923 <59521477+HunterK923@users.noreply.github.com>
2022-09-30 15:12:38 -0400

    Update script.js

--- client/Tetris/script.js
+++ client/Tetris/script.js
@@ -1,57 +1,56 @@
-//variables
-document.addEventListener('DOmContentLoaded', ()=> {
-    const grid = document.querySelector('.grid')
-    let squares = Array.from(document.querySelectorAll('.grid div'))
-    const scoreDisplay = document.querySelector('#score')
-    const startBtn = document.querySelector('#start-button')
-    const width = 10
+// variables
+document.addEventListener('DOmContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  const squares = Array.from(document.querySelectorAll('.grid div'));
+  const scoreDisplay = document.querySelector('#score');
+  const startBtn = document.querySelector('#start-button');
+  const width = 10;
 
-    //The Tetrominoes
-    const lTetromino = [
-        [1, width + 1, width * 2 + 1, 2],
-        [width, width + 1, width + 2, width * 2 + 2],
-        [1, width + 1, width * 2 + 1, width * 2],
-        [width, width * 2, width * 2 + 1, width * 2 + 2]
-    ]
+  // The Tetrominoes
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
+  ];
 
-    const zTetromino = [
-        [0, width ,width + 1,width *2 + 1],
-        [width + 1, width + 2,width * 2, width * 2 + 1],
-        [0, width, width + 1 , width * 2 + 1],
-        [width + 1, width + 2, width * 2, width * 2 + 1]
-    ]
-    
-    const tTetromino = [
-        [1, width , width + 1, width + 2],
-        [1, width + 1, width + 2, width * 2 + 1],
-        [width, width + 1, width + 2, width * 2 + 1],
-        [1, width, width + 1, width * 2 + 1]
-    ]
-    
-    const oTetromino = [
-        [0, 1, width , width + 1],
-        [0, 1, width, width + 1],
-        [0, 1, width, width + 1],
-        [0, 1, width, width + 1]
-    ]
-    
-    const iTetromino = [
-        [1 , width + 1, width * 2 + 1, width * 3 + 1],
-        [width, width + 1, width + 2, width + 3],
-        [1, width + 1, width * 2 + 1 ,width * 3 + 1],
-        [width, width + 1, width + 2, width + 3]
-    ]
-    
-    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1]
+  ];
 
-    let currentPosition = 4
-    let current = theTetrominoes[0][0]
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+  ];
 
-    //draw the first rotation in the firest tetromino
-    function draw(){
-        current.forEach(index => {
-            squares[currentPosition + index].classList.add('tetromino')
-        })
-    }
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ];
 
-})
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ];
+
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+  const currentPosition = 4;
+  const current = theTetrominoes[0][0];
+
+  // draw the first rotation in the firest tetromino
+  function draw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.add('tetromino');
+    });
+  }
+});
