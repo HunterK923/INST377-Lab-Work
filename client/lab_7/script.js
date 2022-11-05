7216431cc0c23f864b357d229ac3696f064d67de
HunterK923 <59521477+HunterK923@users.noreply.github.com>
2022-11-04 20:46:56 -0400

    Stuff

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,4 +1,4 @@
- /* eslint-disable max-len */
+/* eslint-disable max-len */
 
 /*
   Hook this script to index.html
@@ -23,7 +23,7 @@ function injectHTML(list) {
 
   const listEl = document.createElement('ol');
   target.appendChild(listEl);
-  list.forEach(item => {
+  list.forEach((item) => {
     const el = document.createElement('li');
     el.innerText = item.name;
     listEl.appendChild(el);
@@ -72,6 +72,15 @@ function processRestaurants(list) {
   */
 }
 
+function filterList(array, filterInputValue) {
+  const newArray = array.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = filterInputValue.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  });
+  return newArray;
+}
+
 async function mainEvent() {
   /*
     ## Main Event
@@ -110,12 +119,20 @@ async function mainEvent() {
   console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
 
   // This IF statement ensures we can't do anything if we don't have information yet
-  if (arrayFromJson.data?.length > 0) { // the question mark in this means "if this is set at all"
+  if (arrayFromJson.data?.length > 0) {
     submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
 
     // H ide the load animation
     loadAnimation.classList.remove('lds-ellipsis');
     loadAnimation.classList.add('lds-ellipsis_hidden');
+
+    let currentList = [];
+
+    form.addEventListener('input', (event) => {
+      console.log('input', event.target.value);
+      currentList = filterList(currentList, event.target.value);
+      injectHTML(currentList);
+    });
     // And here's an eventListener! It's listening for a "submit" button specifically being clicked
     // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
     form.addEventListener('submit', (submitEvent) => {
@@ -123,11 +140,11 @@ async function mainEvent() {
       submitEvent.preventDefault();
 
       // This constant will have the value of your 15-restaurant collection when it processes
-      const restaurantList = processRestaurants(arrayFromJson.data);
-      console.log(restaurantList);
+      currentList = processRestaurants(arrayFromJson.data);
+      console.log(currentList);
 
       // And this function call will perform the "side effect" of injecting the HTML list for you
-      injectHTML(restaurantList);
+      injectHTML(currentList);
 
       // By separating the functions, we open the possibility of regenerating the list
       // without having to retrieve fresh data every time
